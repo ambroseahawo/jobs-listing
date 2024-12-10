b52825bd1c52b4944d755546c4b374b223576f5b
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-12-10 16:45:52 +0300

    Initial nextjs commit

--- home/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from users.models import Education, Industry
-
-admin.site.register(Education)
-admin.site.register(Industry)
--- users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from.models import Account, Experience, Media
-
-admin.site.register(Experience)
-admin.site.register(Media)
